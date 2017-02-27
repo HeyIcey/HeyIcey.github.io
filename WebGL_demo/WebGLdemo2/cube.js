d23d6d443ab9df9ce43af444a68c9e30c8f2a8de
Icey <280773309@qq.com>
2017-02-27 13:32:06 +0800

    update

--- WebGL_demo/WebGLdemo2/cube.js
+++ WebGL_demo/WebGLdemo2/cube.js
@@ -1,59 +1,47 @@
+var scene = null;
+var camera = null;
+var renderer = null;
+var cube = null;
+var alpha = null;
+var plane = null;
 function init() {
     // renderer
-    var renderer = new THREE.WebGLRenderer({
-        canvas: document.getElementById('mainCanvas')
-    });
-    renderer.shadowMapEnabled = true;
+    renderer = new THREE.WebGLRenderer({canvas: document.getElementById('mainCanvas') });
+    renderer.setClearColor(0x666666); 
+    renderer.shadowMap.enabled = true;
     renderer.shadowMapSoft = true;
 
-    renderer.setClearColor(0x666666); 
-    renderer.shadowMapEnabled = true; //在初始化时，告诉渲染器渲染阴影
     // scene
     var scene = new THREE.Scene();
 
-    // light
-    var light = new THREE.SpotLight(0xffffff, 1, 100, Math.PI/6, 25);
-    light.position.set(5, 15, 9);
-    light.castShadow = true;
-
-    light.shadowCameraNear = 2;
-    light.shadowCameraFar = 10;
-    light.shadowCameraFov = 30;
-    light.shadowCameraVisible = true;
-    light.shadowMapWidth = 1024;
-    light.shadowMapHeight = 1024;
-    light.shadowDarkness = 0.3;
-    
-    scene.add(light);
-
     // camera
-    var camera = new THREE.OrthographicCamera(-2, 2, 1.5, -1.5, 5, 20);
-    camera.position.set(5, 5, 5);
+    var camera = new THREE.PerspectiveCamera(20, 400/300, 1, 50);
+    camera.position.set(6, 8, 8);
     camera.lookAt(new THREE.Vector3(0, 0, 0));
     scene.add(camera);
 
-    var plane = new THREE.Mesh(new THREE.PlaneGeometry(8, 8, 16, 16),
-            new THREE.MeshLambertMaterial({color: 0x9D74B2}));
-    plane.rotation.x = -Math.PI / 2;
+    // a plane
+    plane = new THREE.Mesh(new THREE.PlaneGeometry(8,8), new THREE.MeshLambertMaterial({color: 0xeeeeee}));
+    plane.rotation.x = -Math.PI*0.5;
     plane.position.y = -1;
     plane.receiveShadow = true;
     scene.add(plane);
-    
-    // material
-    var material = new THREE.MeshLambertMaterial({
-                color: 0xC9C9C9
-            });
+
+
     //carBody 
-    var carBody = new THREE.CubeGeometry(2, 1, 1);
-    var cube = new THREE.Mesh(carBody,material);
+    cube = new THREE.Mesh(new THREE.CubeGeometry(2, 1, 1), new THREE.MeshLambertMaterial({  color: 0xC9C9C9 }) );
+    cube.castShadow = true;
+    cube.reveiveShadow = true;
     scene.add(cube);
+
 
     // // carCircle
     var carCircle = new THREE.TorusGeometry(0.2, 0.06, 52, 48);
-    var carCircle1 = new THREE.Mesh(carCircle, material);
-    var carCircle2 = new THREE.Mesh(carCircle, material);
-    var carCircle3 = new THREE.Mesh(carCircle, material);
-    var carCircle4 = new THREE.Mesh(carCircle, material);
+
+    var carCircle1 = new THREE.Mesh(carCircle, new THREE.MeshLambertMaterial({color: 0xC9C9C9}));
+    var carCircle2 = new THREE.Mesh(carCircle,new THREE.MeshLambertMaterial({  color: 0xC9C9C9}));
+    var carCircle3 = new THREE.Mesh(carCircle,new THREE.MeshLambertMaterial({ color: 0xC9C9C9}));
+    var carCircle4 = new THREE.Mesh(carCircle, new THREE.MeshLambertMaterial({color: 0xC9C9C9}));
     scene.add(carCircle1);
     scene.add(carCircle2);
     scene.add(carCircle3);
@@ -63,14 +51,18 @@ function init() {
     carCircle3.position.set(-0.5, -0.5, -0.5);
     carCircle4.position.set(0.5, -0.5, -0.5);
 
-    //对于光源以及所有要产生阴影的物体调用
-    cube.castShadow = true;
-    carCircle1.castShadow = true;
-    carCircle2.castShadow = true;
-    carCircle3.castShadow = true;
-    carCircle4.castShadow = true;
+    // spot light
+    var light = new THREE.SpotLight(0xffffff, 1.5, 200, Math.PI/6, 25);
+    light.position.set(2, 3, 10);
+    light.target = cube;
+    light.target = carCircle1;
+    light.target = carCircle2;
+    light.target = carCircle3;
+    light.target = carCircle4;
+    light.castShadow = true;
+    scene.add(light);
 
-
+    
     // render
     renderer.render(scene, camera);
 }
